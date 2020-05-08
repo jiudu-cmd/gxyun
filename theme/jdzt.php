a6d1017378140b8464dbb4258018e188cdb63d12
jiuduyun <64449053+jiudu-cmd@users.noreply.github.com>
2020-05-08 16:20:56 +0800

    Update jdzt.php

--- theme/jdzt.php
+++ theme/jdzt.php
@@ -11,11 +11,11 @@
     <link rel="shortcut icon" href="<?php echo $_SERVER['base_disk_path'];?>favicon.ico" type="image/x-icon" />
     <style type="text/css">
         body{margin:0;padding:0;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:1em;color:#000;background-color:#F9FAFB;}
-        .topbar{width:100%;height:40px;background-color:#00ae9d;}
- &nbsp;
+    &nbsp;
    	    <center>
-        <iframe width="350" scrolling="no" height="20" frameborder="0" allowtransparency="true" src="//i.tianqi.com/index.php?c=code&id=11&color=#fffffb&bgc=%23&icon=1&site=10"></iframe>
+        <iframe width="350" scrolling="no" height="20" frameborder="0" allowtransparency="true" src="//i.tianqi.com/index.php?c=code&id=11&color=#fffffb&bgc=%23&icon=1&site=10&background-color:#00ae9d"></iframe>
         </center>
+        .topbar{width:100%;height:40px;background-color:#00ae9d;}
         .topbar .login{display:block;line-height:40px;color:#ffffff;margin-left:10%;float:left;}
         .topbar .login ion-icon{display:block;height:40px;line-height:40px;margin-right:5px;float:left;}
         .topbar .operate{display:block;line-height:40px;color:#ffffff;margin-left:10%;float:left;}
